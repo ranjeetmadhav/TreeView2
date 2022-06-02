d66307045160d448d2ce899ac33117c2472f5fe6
ranjeetmadhav <ranjeetmadhav_co20b6_07@dtu.ac.in>
2022-06-02 14:52:59 +0530

    UPdated

---
+++ treeview/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TreeList from './components/TreeData';
+
+function App() {
+  return (
+    <div >
+    
+       
+     <TreeList/> 
+     
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ treeview/src/components/Tree/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./index.css";
+const Tree = ({ data = [] }) => {
+    return (
+      <div className="d-tree">
+        <ul className="d-flex d-tree-container flex-column">
+          {data.map((tree) => (
+            <TreeNode node={tree} />
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+
+  const TreeNode = ({ node }) => {
+    const [childVisible, setChildVisiblity] = useState(false);
+  
+    const hasChild = node.children ? true : false;
+  
+    return (
+      <li className="d-tree-node border-0">
+        <div className="d-flex" onClick={(e) => setChildVisiblity((v) => !v)}>
+          {hasChild && (
+            <div
+              className={`d-inline d-tree-toggler ${
+                childVisible ? "active" : ""
+              }`}
+            >
+        <i class="bi bi-app-indicator"></i>
+            </div>
+          )}
+  
+          <div className="col d-tree-head">
+            <i className={`mr-1 ${node.icon}`}> </i>
+            {node.label}
+          </div>
+        </div>
+  
+        {hasChild && childVisible && (
+          <div className="d-tree-content">
+            <ul className="d-flex d-tree-container flex-column">
+              <Tree data={node.children} />
+            </ul>
+          </div>
+        )}
+      </li>
+    );
+  };
+  
+
+export default Tree;
